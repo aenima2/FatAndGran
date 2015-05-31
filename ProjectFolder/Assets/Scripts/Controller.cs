98278d82e4ce1160f2bc393e8e0405956a09b8db
Hannes <hannes.lidbeck@futuregames.se>
2015-05-31 10:57:40 +0200

    level 01 set up

---
+++ ProjectFolder/Assets/Scripts/Controller.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controller : MonoBehaviour {
+
+    public Transform m_targetTf;
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+
+        
+    }
+}
