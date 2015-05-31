1ccf1e3706d59a0b0650eae4ce7121b423913e9a
Hannes <hannes.lidbeck@futuregames.se>
2015-05-31 11:15:43 +0200

    basic input and movement in place

--- ProjectFolder/Assets/Scripts/Controller.cs
+++ ProjectFolder/Assets/Scripts/Controller.cs
@@ -1,9 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public enum PInput { Keyboard01, Keyboard02, Mouse };
+
 public class Controller : MonoBehaviour {
 
+    public PInput m_playerInput;
+
     public Transform m_targetTf;
+
+    public float m_speed = 10f;
+
+    float inputX = 0.0f;
+    float inputZ = 0.0f;
 
 
     void Start()
@@ -13,9 +22,20 @@ public class Controller : MonoBehaviour {
 
     void Update()
     {
-        float inputX = Input.GetAxis("Horizontal");
-        float inputZ = Input.GetAxis("Vertical");
+        switch (m_playerInput)
+        {
+            case PInput.Keyboard01:
+                inputX = Input.GetAxis("Horizontal");
+                inputZ = Input.GetAxis("Vertical");
+                break;
+            case PInput.Keyboard02:
+                inputX = Input.GetAxis("Horizontal02");
+                inputZ = Input.GetAxis("Vertical02");
+                break;
+            case PInput.Mouse:
+                break;
+        }
 
-        //transform.Translate(Vector3.right * inputX * Time.deltaTime, 0f, Vector3.forward * inputZ * Time.deltaTime));
+        transform.Translate(new Vector3(inputX, 0, inputZ) * m_speed * Time.deltaTime);
     }
 }
