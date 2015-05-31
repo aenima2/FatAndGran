a38670161475827144fcdc792ea5206d63861107
Hannes <hannes.lidbeck@futuregames.se>
2015-05-31 11:05:10 +0200

    meta files commit

--- ProjectFolder/Assets/Scripts/Controller.cs
+++ ProjectFolder/Assets/Scripts/Controller.cs
@@ -8,14 +8,14 @@ public class Controller : MonoBehaviour {
 
     void Start()
     {
-
+        m_targetTf = transform.FindChild("Mesh");
     }
 
     void Update()
     {
         float inputX = Input.GetAxis("Horizontal");
-        float inputY = Input.GetAxis("Vertical");
+        float inputZ = Input.GetAxis("Vertical");
 
-        
+        //transform.Translate(Vector3.right * inputX * Time.deltaTime, 0f, Vector3.forward * inputZ * Time.deltaTime));
     }
 }
